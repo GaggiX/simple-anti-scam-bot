296c6a0462caa83662811b218fe8faadb9e379e4
GaggiX <fedegaggix@gmail.com>
2020-12-29 22:26:51 +0100

    remove possible race condition with files

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use reqwest;
 use teloxide::prelude::*;
 
 use std::env;
-use std::fs::File;
+use std::fs::{File, remove_file};
 use std::io::Write;
 use std::process::Command;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[tokio::main]
 async fn main() {
@@ -22,12 +23,13 @@ async fn main() {
             None => return ResponseResult::<()>::Ok(()),
         };
 
-        save_image(&message, photo_id).await.unwrap();
-        let scam = scam_test().unwrap();
+        let image_name = save_image(&message, photo_id).await.unwrap();
+        let scam = scam_test(image_name).unwrap();
 
         if scam {
             message.delete_message().send().await?;
         }
+        
         ResponseResult::<()>::Ok(())
     })
     .await;
@@ -36,8 +38,9 @@ async fn main() {
 async fn save_image(
     message: &UpdateWithCx<Message>,
     photo_id: &String,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<String, Box<dyn std::error::Error>> {
     let photo_path = message.bot.get_file(photo_id).send().await?.file_path;
+    let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_nanos();
 
     let bytes_image = reqwest::get(&format!(
         "https://api.telegram.org/file/bot{}/{}",
@@ -48,18 +51,20 @@ async fn save_image(
     .bytes()
     .await?;
 
-    let mut file = File::create("image")?;
+    let image_name = format!("image-{}", timestamp);
+    let mut file = File::create(&image_name)?;
     file.write_all(&bytes_image)?;
-    Ok(())
+    Ok(image_name)
 }
 
-fn scam_test() -> Result<bool, Box<dyn std::error::Error>> {
+fn scam_test(image_name: String) -> Result<bool, Box<dyn std::error::Error>> {
     let text = String::from_utf8(
         Command::new("tesseract")
-            .args(&["image", "-"])
+            .args(&[&image_name, "-"])
             .output()?
             .stdout,
     )?;
 
+    remove_file(image_name)?;
     Ok(text.contains("bitcoin") && text.contains("blockchain") && text.contains("giveaway"))
 }
